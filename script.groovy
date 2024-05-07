ca63bca9bfda213a426511202f2994c33c91ea77
ahmedihab22 <84748900+ahmedihab22@users.noreply.github.com>
2024-05-07 14:11:02 +0300

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -1,1 +1,24 @@
+def buildJar() {
+    echo "building the application..."
+    sh 'mvn package'
+} 
 
+def buildImage() {
+     try {
+    echo "building the docker image..."
+    withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASSWORD', usernameVariable: 'USERNAME')]) {
+        sh ' docker build -t elghetani/jenkins:jma-2.0 .'
+        sh " echo $PASSWORD | docker login -u $USERNAME --password-stdin"
+        sh ' docker push elghetani/jenkins:jma-2.0'
+    }
+     }
+    catch (err) {                                        
+                unstable(message: "${STAGE_NAME} is unstable")
+            }
+} 
+
+def deployApp() {
+    echo 'deploying the application...'
+} 
+
+return this
